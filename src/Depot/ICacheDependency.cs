1a5ca258dd8adf17892efbe417b327015a2f233c
danielcrenna <daniel.crenna@gmail.com>
2011-05-16 08:09:00 -0400

    Initial import and feature set (dependency support not included yet)

---
+++ src/Depot/ICacheDependency.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Depot
+{
+    public interface ICacheDependency : IDisposable
+    {
+        string Id { get; }
+        bool ShouldInvalidate { get; }
+        DateTime LastModified { get; }
+    }
+}
