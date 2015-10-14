ebdbcb78b6c523db5bbe7e1bbe3b7ad1d79ba1f9
Daniel Crenna <daniel@conatuscreative.com>
2015-10-14 13:06:48 -0400

    added tophat

---
+++ tophat/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+
+namespace tophat
+{
+    public abstract class ConnectionFactory : IConnectionFactory
+    {
+        public abstract DbConnection CreateConnection();
+
+        public DbConnection GetUnitOfWorkScopedConnection()
+        {
+            var context = GetContext();
+            return context ? .Connection;
+        }
+
+        public void Reset()
+        {
+            var context = GetContext();
+            context.Dispose();
+        }
+
+        private static DataContext GetContext()
+        {
+            var context = Database.Container.Resolve<DataContext>();
+            return context;
+        }
+
+        public bool IsActive => GetContext().IsActive;
+
+        public string ConnectionString { get; set; }
+    }
+}
