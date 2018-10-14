bd3663baf857e8aa6aebca6101b7b7e73220ad61
Daniel Crenna <daniel@hq.io>
2018-10-14 19:44:14 -0400

    remove sample application

--- src/Site.AspNetCore21.Views/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Site.AspNetCore21.Views.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
