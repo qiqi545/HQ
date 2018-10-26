1aaf54cc9582b1cef3e62eb27621dc627cde39de
Daniel Crenna <daniel@hq.io>
2018-10-25 23:58:26 -0400

    setup simple benchmark project

---
+++ src/HQ.Harmony.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace HQ.Harmony.Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<ResolveBenchmark>();
+		}
+	}
+}
