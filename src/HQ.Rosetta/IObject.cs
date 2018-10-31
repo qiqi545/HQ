881e4524b5482a0605f731b0405627d7684acbba
Daniel Crenna <daniel@hq.io>
2018-10-31 02:11:47 -0400

    Add CreatedAt to IObject
    unlike "UpdatedAt", this should be as shelf-stable as the ID

--- src/HQ.Rosetta/IObject.cs
+++ src/HQ.Rosetta/IObject.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) HQ.IO Corporation. All rights reserved.
 // Licensed under the Reciprocal Public License, Version 1.5. See LICENSE.md in the project root for license terms.
 
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace HQ.Rosetta
 {
 	public interface IObject
 	{
-		[Key] long Id { get; set; }
+		[Key] long Id { get; }
+		DateTimeOffset CreatedAt { get; }
 	}
 }
