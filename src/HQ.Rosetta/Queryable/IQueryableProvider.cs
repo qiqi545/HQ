ef1f26a3cff1132495c92e581d67d9ce33556783
Daniel Crenna <daniel@hq.io>
2018-11-04 16:18:32 -0500

    Add contracts for safe queryables

---
+++ src/HQ.Rosetta/Queryable/IQueryableProvider.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) HQ Corporation. All rights reserved.
+// Licensed under the Reciprocal Public License, Version 1.5. See LICENSE.md in the project root for license terms.
+
+using System;
+using System.Linq;
+
+namespace HQ.Rosetta.Queryable
+{
+	public interface IQueryableProvider<T>
+	{
+		/// <summary>
+		///     Determines whether to throw a <see cref="NotSupportedException" /> if <see cref="Queryable" /> is accessed.
+		///     If this is <code>false</code>, then developers may potentially access a <see cref="Queryable" /> that is
+		///     unpredictable, or, if none is available, may materialize the entire underlying collection in order to
+		///     perform a query.
+		/// </summary>
+		bool ThrowOnQueryableAccess { get; }
+
+		IQueryable<T> Queryable { get; }
+		ISafeQueryable<T> SafeQueryable { get; }
+	}
+}
---
+++ src/HQ.Rosetta/Queryable/ISafeQueryable.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) HQ Corporation. All rights reserved.
+// Licensed under the Reciprocal Public License, Version 1.5. See LICENSE.md in the project root for license terms.
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HQ.Rosetta.Queryable
+{
+	/// <summary>
+	///     Provides implementation-safe expression predicates when <see cref="IQueryable" /> access is undesirable or
+	///     unstable.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface ISafeQueryable<T>
+	{
+		Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+		T SingleOrDefault(Expression<Func<T, bool>> predicate);
+		Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+		T FirstOrDefault(Expression<Func<T, bool>> predicate);
+	}
+}
