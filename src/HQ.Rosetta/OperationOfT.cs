e1f9475b8ce986f1fa9f629061f07c109027f6c5
Daniel Crenna <daniel@hq.io>
2018-11-08 12:41:13 -0500

    prep for packaging

--- src/HQ.Rosetta/OperationOfT.cs
+++ src/HQ.Rosetta/OperationOfT.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections.Generic;
+﻿#region LICENSE
+
+// Unless explicitly acquired and licensed from Licensor under another
+// license, the contents of this file are subject to the Reciprocal Public
+// License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+// and You may not copy or use this file in either source code or executable
+// form, except in compliance with the terms and conditions of the RPL.
+// 
+// All software distributed under the RPL is provided strictly on an "AS
+// IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+// LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+// LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+// language governing rights and limitations under the RPL.
+
+#endregion
+
+using System.Collections.Generic;
 
 namespace HQ.Rosetta
 {
 	public sealed class Operation<T> : Operation
 	{
-		public T Data { get; set; }
-
-		public Operation(T data) : this(data, null) { }
+		public Operation(T data) : this(data, null)
+		{
+		}
 
 		public Operation(T data, IEnumerable<Error> errors) : base(errors)
 		{
 			Data = data;
 		}
+
+		public T Data { get; set; }
 	}
 }
