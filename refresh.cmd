140b92d1b44a55c501f091672fda7ebb1890fffe
Daniel Crenna <daniel@hq.io>
2018-11-08 16:04:02 -0500

    Update refresh.cmd

--- refresh.cmd
+++ refresh.cmd
@@ -1,3 +1,10 @@
-FOR /D %%d IN ("D:\Src\HQ\HQ\src\HQ") DO rmdir /s /q "%%d"
-	
-	
+@ECHO OFF
+
+REM update all submodules to use tips of master branches
+git submodule foreach git pull origin master
+
+REM recursively update the tips of master branches
+git submodule update --recursive --remote
+
+REM Remove all source shadow copies
+FOR /D %%p IN ("D:\Src\HQ\HQ\src\HQ\*.*") DO rmdir "%%p" /s /q
