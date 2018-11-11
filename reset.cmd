dbdbdbafe2cea58c63823658dd80b374848d3f8d
Daniel Crenna <daniel@hq.io>
2018-11-11 04:04:07 -0500

    update to latest

--- reset.cmd
+++ reset.cmd
@@ -1,12 +1,4 @@
 @ECHO OFF
 
-REM resets all shadow copied files after a source deployment
-
-REM update all submodules to use tips of master branches
-git submodule foreach git pull origin master
-
-REM recursively update the tips of master branches
-git submodule update --recursive --remote
-
 REM remove all source shadow copies
 FOR /D %%p IN ("src\HQ\*.*") DO rmdir "%%p" /s /q
