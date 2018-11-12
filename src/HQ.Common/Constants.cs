69f2e2480623ca51c8f625a9b9199e3c64680bc8
Daniel Crenna <daniel@hq.io>
2018-11-12 16:57:10 -0500

    start new constants wrapper class

---
+++ src/HQ.Common/Constants.cs
@@ -0,0 +1,12 @@
+namespace HQ.Common
+{
+    public static class Constants
+    {
+        public static class Sql
+        {
+            public const char ParentAlias = 'p';
+            public const char ChildAlias = 'c';
+            public const string SetSuffix = "_set";
+        }
+    }
+}
