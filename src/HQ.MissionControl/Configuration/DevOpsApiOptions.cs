fadaf6612d026e86f5efcbdeae9e19e6c053c55d
Daniel Crenna <daniel@hq.io>
2018-11-13 16:01:36 -0500

    cleanup constants and formatting

--- src/HQ.MissionControl/Configuration/DevOpsApiOptions.cs
+++ src/HQ.MissionControl/Configuration/DevOpsApiOptions.cs
@@ -1,21 +1,35 @@
-﻿// Copyright (c) HQ.IO Corporation. All rights reserved.
-// Licensed under the Reciprocal Public License, Version 1.5. See LICENSE.md in the project root for license terms.
+#region LICENSE
+
+// Unless explicitly acquired and licensed from Licensor under another
+// license, the contents of this file are subject to the Reciprocal Public
+// License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+// and You may not copy or use this file in either source code or executable
+// form, except in compliance with the terms and conditions of the RPL.
+// 
+// All software distributed under the RPL is provided strictly on an "AS
+// IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+// LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+// LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+// language governing rights and limitations under the RPL.
+
+#endregion
 
 using HQ.Common;
 
 namespace HQ.MissionControl.Configuration
 {
-	public class DevOpsApiOptions
-	{
-		public string RootPath { get; set; } = "/ops";
+    public class DevOpsApiOptions
+    {
+        public string RootPath { get; set; } = "/ops";
 
-		public bool EnableRouteDebugging { get; set; } = true;
-		public string RouteDebuggingPath { get; set; } = "/routes";
+        public bool EnableRouteDebugging { get; set; } = true;
+        public string RouteDebuggingPath { get; set; } = "/routes";
 
-		public bool EnableRequestProfiling { get; set; } = true;
-		public string RequestProfilingHeader { get; set; } = HqHeaders.ServiceTiming;
+        public bool EnableRequestProfiling { get; set; } = true;
+        public string RequestProfilingHeader { get; set; } = Constants.HttpHeaders.ServiceTiming;
 
-		public bool EnableEnvironmentEndpoint { get; set; } = true;
-		public string EnvironmentEndpointPath { get; set; } = "/env";
-	}
+        public bool EnableEnvironmentEndpoint { get; set; } = true;
+        public string EnvironmentEndpointPath { get; set; } = "/env";
+    }
 }
--- src/HQ.MissionControl/Use.cs
+++ src/HQ.MissionControl/Use.cs
@@ -1,5 +1,19 @@
-﻿// Copyright (c) HQ.IO Corporation. All rights reserved.
-// Licensed under the Reciprocal Public License, Version 1.5. See LICENSE.md in the project root for license terms.
+#region LICENSE
+
+// Unless explicitly acquired and licensed from Licensor under another
+// license, the contents of this file are subject to the Reciprocal Public
+// License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+// and You may not copy or use this file in either source code or executable
+// form, except in compliance with the terms and conditions of the RPL.
+// 
+// All software distributed under the RPL is provided strictly on an "AS
+// IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+// LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+// LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+// language governing rights and limitations under the RPL.
+
+#endregion
 
 using System;
 using System.Collections.Generic;
@@ -28,214 +42,213 @@ using Newtonsoft.Json;
 
 namespace HQ.MissionControl
 {
-	public static class Use
-	{
-		public static IApplicationBuilder UseDevOpsApi(this IApplicationBuilder app,
-			Action<IRouteBuilder> configureRoutes = null)
-		{
-			Bootstrap.EnsureInitialized();
+    public static class Use
+    {
+        public static IApplicationBuilder UseDevOpsApi(this IApplicationBuilder app,
+            Action<IRouteBuilder> configureRoutes = null)
+        {
+            Bootstrap.EnsureInitialized();
 
-			app.UseRequestProfiling();
-			app.UseEnvironmentEndpoint();
+            app.UseRequestProfiling();
+            app.UseEnvironmentEndpoint();
 
-			var options = app.ApplicationServices.GetService<IOptions<DevOpsApiOptions>>();
-			var enabled = options.Value.EnableRouteDebugging;
-			if (options?.Value != null && enabled)
-			{
-				object Map(ActionDescriptor descriptor)
-				{
-					var controller = descriptor.RouteValues["Controller"];
-					var action = descriptor.RouteValues["Action"];
-					var constraints = descriptor.ActionConstraints;
-					var filters = descriptor.FilterDescriptors.OrderBy(x => x.Order).ThenBy(x => x.Scope)
-						.Select(x => x.Filter.GetType().Name);
+            var options = app.ApplicationServices.GetService<IOptions<DevOpsApiOptions>>();
+            var enabled = options.Value.EnableRouteDebugging;
+            if (options?.Value != null && enabled)
+            {
+                object Map(ActionDescriptor descriptor)
+                {
+                    var controller = descriptor.RouteValues["Controller"];
+                    var action = descriptor.RouteValues["Action"];
+                    var constraints = descriptor.ActionConstraints;
+                    var filters = descriptor.FilterDescriptors.OrderBy(x => x.Order).ThenBy(x => x.Scope)
+                        .Select(x => x.Filter.GetType().Name);
 
-					return new
-					{
-						descriptor.Id,
-						Path = $"{controller}/{action}",
-						Action = action,
-						descriptor.DisplayName,
-						descriptor.AttributeRouteInfo?.Template,
-						descriptor.AttributeRouteInfo?.Name,
-						Filters = filters,
-						Constraints = constraints
-					};
-				}
+                    return new
+                    {
+                        descriptor.Id,
+                        Path = $"{controller}/{action}",
+                        Action = action,
+                        descriptor.DisplayName,
+                        descriptor.AttributeRouteInfo?.Template,
+                        descriptor.AttributeRouteInfo?.Name,
+                        Filters = filters,
+                        Constraints = constraints
+                    };
+                }
 
-				app.Map(options.Value.RootPath ?? string.Empty, x =>
-				{
-					x.UseMvc(routes =>
-					{
-						if (options.Value.EnableRouteDebugging)
-						{
-							Task GetRoutesHandler(HttpContext context)
-							{
-								return WriteResultAsJson(app, context, context.RequestServices.GetRequiredService<IActionDescriptorCollectionProvider>().ActionDescriptors.Items.Select(Map));
-							}
+                app.Map(options.Value.RootPath ?? string.Empty, x =>
+                {
+                    x.UseMvc(routes =>
+                    {
+                        if (options.Value.EnableRouteDebugging)
+                        {
+                            Task GetRoutesHandler(HttpContext context)
+                            {
+                                return WriteResultAsJson(app, context,
+                                    context.RequestServices.GetRequiredService<IActionDescriptorCollectionProvider>()
+                                        .ActionDescriptors.Items.Select(Map));
+                            }
 
-							routes.MapGet(options.Value.RouteDebuggingPath ?? "routes", GetRoutesHandler);
-						}
+                            routes.MapGet(options.Value.RouteDebuggingPath ?? "routes", GetRoutesHandler);
+                        }
 
-						configureRoutes?.Invoke(routes);
-					});
-				});
-			}
+                        configureRoutes?.Invoke(routes);
+                    });
+                });
+            }
 
-			return app;
-		}
+            return app;
+        }
 
-		internal static IApplicationBuilder UseRequestProfiling(this IApplicationBuilder app)
-		{
-			return app.Use(async (context, next) =>
-			{
-				var options = context.RequestServices.GetService<IOptions<DevOpsApiOptions>>();
-				if (options?.Value != null && options.Value.EnableRequestProfiling)
-				{
-					var sw = PooledStopwatch.StartInstance();
+        internal static IApplicationBuilder UseRequestProfiling(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, next) =>
+            {
+                var options = context.RequestServices.GetService<IOptions<DevOpsApiOptions>>();
+                if (options?.Value != null && options.Value.EnableRequestProfiling)
+                {
+                    var sw = PooledStopwatch.StartInstance();
 
-					context.Response.OnStarting(() =>
-					{
-						sw.Free();
-						var header = options.Value.RequestProfilingHeader ?? HqHeaders.ServiceTiming;
-						context.Response.Headers.Add(header, $"{sw.Elapsed.TotalMilliseconds}");
-						return Task.CompletedTask;
-					});
-				}
-				
-				await next();
-			});
-		}
+                    context.Response.OnStarting(() =>
+                    {
+                        sw.Free();
+                        var header = options.Value.RequestProfilingHeader ?? Constants.HttpHeaders.ServiceTiming;
+                        context.Response.Headers.Add(header, $"{sw.Elapsed.TotalMilliseconds}");
+                        return Task.CompletedTask;
+                    });
+                }
 
-		internal static IApplicationBuilder UseEnvironmentEndpoint(this IApplicationBuilder app)
-		{
-			string GetPlatform()
-			{
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-					return "Linux";
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-					return "Windows";
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-					return "OSX";
-				return "Unknown";
-			}
+                await next();
+            });
+        }
 
-			// See: https://github.com/dotnet/BenchmarkDotNet/issues/448#issuecomment-308424100
-			string GetNetCoreVersion()
-			{
-				var assembly = typeof(GCSettings).Assembly;
-				var assemblyPath = assembly.CodeBase.Split(new[] {'/', '\\'}, StringSplitOptions.RemoveEmptyEntries);
-				var netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
-				return netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2
-					? assemblyPath[netCoreAppIndex + 1]
-					: null;
-			}
+        internal static IApplicationBuilder UseEnvironmentEndpoint(this IApplicationBuilder app)
+        {
+            string GetPlatform()
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    return "Linux";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return "Windows";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    return "OSX";
+                return "Unknown";
+            }
 
-			return app.Use(async (context, next) =>
-			{
-				var options = context.RequestServices.GetRequiredService<IOptions<DevOpsApiOptions>>();
-				var template = options.Value.RootPath + options.Value.EnvironmentEndpointPath;
+            // See: https://github.com/dotnet/BenchmarkDotNet/issues/448#issuecomment-308424100
+            string GetNetCoreVersion()
+            {
+                var assembly = typeof(GCSettings).Assembly;
+                var assemblyPath = assembly.CodeBase.Split(new[] {'/', '\\'}, StringSplitOptions.RemoveEmptyEntries);
+                var netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
+                return netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2
+                    ? assemblyPath[netCoreAppIndex + 1]
+                    : null;
+            }
 
-				if (options.Value != null &&
-				    options.Value.EnableEnvironmentEndpoint &&
-				    context.Request.Path.Value.StartsWith(template))
-				{
-					var hosting = context.RequestServices.GetRequiredService<IHostingEnvironment>();
-					var config = context.RequestServices.GetRequiredService<IConfiguration>();
+            return app.Use(async (context, next) =>
+            {
+                var options = context.RequestServices.GetRequiredService<IOptions<DevOpsApiOptions>>();
+                var template = options.Value.RootPath + options.Value.EnvironmentEndpointPath;
 
-					var process = Process.GetCurrentProcess();
-					var hostName = Dns.GetHostName();
-					var hostEntry = Dns.GetHostEntry(hostName);
+                if (options.Value != null &&
+                    options.Value.EnableEnvironmentEndpoint &&
+                    context.Request.Path.Value.StartsWith(template))
+                {
+                    var hosting = context.RequestServices.GetRequiredService<IHostingEnvironment>();
+                    var config = context.RequestServices.GetRequiredService<IConfiguration>();
 
-					IDictionary<string, object> configuration = new ExpandoObject();
-					foreach (var entry in config.AsEnumerable())
-						configuration.Add(entry.Key, entry.Value);
+                    var process = Process.GetCurrentProcess();
+                    var hostName = Dns.GetHostName();
+                    var hostEntry = Dns.GetHostEntry(hostName);
 
-					var env = new
-					{
-						Dns = new
-						{
-							HostName = hostName,
-							HostEntry = new
-							{
-								hostEntry.Aliases,
-								Addresses = hostEntry.AddressList.Select(x => x.ToString())
-							}
-						},
-						OperatingSystem = new
-						{
-							Platform = GetPlatform(),
-							Description = RuntimeInformation.OSDescription,
-							Architecture = RuntimeInformation.OSArchitecture,
-							Version = Environment.OSVersion,
-							Is64Bit = Environment.Is64BitOperatingSystem
-						},
-						Process = new
-						{
-							process.Id,
-							process.ProcessName,
-							process.MachineName,
-							Arguments = Environment.GetCommandLineArgs(),
-							Architecture = RuntimeInformation.ProcessArchitecture,
-							MaxWorkingSet = process.MaxWorkingSet.ToInt64(),
-							MinWorkingSet = process.MinWorkingSet.ToInt64(),
-							process.PagedMemorySize64,
-							process.PeakWorkingSet64,
-							process.PrivateMemorySize64,
-							process.StartTime,
-							Is64Bit = Environment.Is64BitProcess
-						},
-						Environment = new
-						{
-							hosting.EnvironmentName,
-							hosting.ApplicationName,
-							hosting.ContentRootPath,
-							hosting.WebRootPath,
-							Environment.CurrentDirectory,
-							Environment.CurrentManagedThreadId
-						},
-						Framework = new
-						{
-							Version = $"{RuntimeInformation.FrameworkDescription}",
-							NetCoreVersion = GetNetCoreVersion(),
-							ClrVersion = Environment.Version.ToString()
-						},
-						Configuration = configuration
-					};
+                    IDictionary<string, object> configuration = new ExpandoObject();
+                    foreach (var entry in config.AsEnumerable())
+                        configuration.Add(entry.Key, entry.Value);
 
-					await WriteResultAsJson(app, context, env);
-					return;
-				}
+                    var env = new
+                    {
+                        Dns = new
+                        {
+                            HostName = hostName,
+                            HostEntry = new
+                            {
+                                hostEntry.Aliases,
+                                Addresses = hostEntry.AddressList.Select(x => x.ToString())
+                            }
+                        },
+                        OperatingSystem = new
+                        {
+                            Platform = GetPlatform(),
+                            Description = RuntimeInformation.OSDescription,
+                            Architecture = RuntimeInformation.OSArchitecture,
+                            Version = Environment.OSVersion,
+                            Is64Bit = Environment.Is64BitOperatingSystem
+                        },
+                        Process = new
+                        {
+                            process.Id,
+                            process.ProcessName,
+                            process.MachineName,
+                            Arguments = Environment.GetCommandLineArgs(),
+                            Architecture = RuntimeInformation.ProcessArchitecture,
+                            MaxWorkingSet = process.MaxWorkingSet.ToInt64(),
+                            MinWorkingSet = process.MinWorkingSet.ToInt64(),
+                            process.PagedMemorySize64,
+                            process.PeakWorkingSet64,
+                            process.PrivateMemorySize64,
+                            process.StartTime,
+                            Is64Bit = Environment.Is64BitProcess
+                        },
+                        Environment = new
+                        {
+                            hosting.EnvironmentName,
+                            hosting.ApplicationName,
+                            hosting.ContentRootPath,
+                            hosting.WebRootPath,
+                            Environment.CurrentDirectory,
+                            Environment.CurrentManagedThreadId
+                        },
+                        Framework = new
+                        {
+                            Version = $"{RuntimeInformation.FrameworkDescription}",
+                            NetCoreVersion = GetNetCoreVersion(),
+                            ClrVersion = Environment.Version.ToString()
+                        },
+                        Configuration = configuration
+                    };
 
-				await next();
-			});
-		}
+                    await WriteResultAsJson(app, context, env);
+                    return;
+                }
 
-		private static async Task WriteResultAsJson(IApplicationBuilder app, HttpContext context, object env)
-		{
-			context.Response.Headers.Add(HttpHeaders.ContentType, MediaTypes.Json);
-			await context.Response.WriteAsync(SerializeObject(app, context, env));
-		}
+                await next();
+            });
+        }
 
-		private static string SerializeObject(IApplicationBuilder app, HttpContext context, object instance)
-		{
-			var serializerSettings = app.ApplicationServices.GetService<JsonSerializerSettings>();
-			if (serializerSettings != null)
-			{
-				if (context.Items[HqContextKeys.JsonMultiCase] is ITextTransform transform)
-				{
-					serializerSettings.ContractResolver =
-						new JsonContractResolver(transform, JsonProcessingDirection.Output);
-				}
-				else
-				{
-					serializerSettings = JsonConvert.DefaultSettings();
-				}
-			}
-			var json = serializerSettings != null
-				? JsonConvert.SerializeObject(instance, serializerSettings)
-				: JsonConvert.SerializeObject(instance);
-			return json;
-		}
-	}
+        private static async Task WriteResultAsJson(IApplicationBuilder app, HttpContext context, object env)
+        {
+            context.Response.Headers.Add(Constants.HttpHeaders.ContentType, Constants.MediaTypes.Json);
+            await context.Response.WriteAsync(SerializeObject(app, context, env));
+        }
+
+        private static string SerializeObject(IApplicationBuilder app, HttpContext context, object instance)
+        {
+            var serializerSettings = app.ApplicationServices.GetService<JsonSerializerSettings>();
+            if (serializerSettings != null)
+            {
+                if (context.Items[Constants.ContextKeys.JsonMultiCase] is ITextTransform transform)
+                    serializerSettings.ContractResolver =
+                        new JsonContractResolver(transform, JsonProcessingDirection.Output);
+                else
+                    serializerSettings = JsonConvert.DefaultSettings();
+            }
+
+            var json = serializerSettings != null
+                ? JsonConvert.SerializeObject(instance, serializerSettings)
+                : JsonConvert.SerializeObject(instance);
+            return json;
+        }
+    }
 }
