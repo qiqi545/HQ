10db89ff2510ff928664e54b3b1d071718bc39ca
Daniel Crenna <daniel@hq.io>
2018-11-13 13:51:49 -0500

    code cleanup and license headers

--- src/HQ.Cohort.Stores.Sql.DocumentDb/CreateIdentitySchema.cs
+++ src/HQ.Cohort.Stores.Sql.DocumentDb/CreateIdentitySchema.cs
@@ -1,3 +1,20 @@
+#region LICENSE
+
+// Unless explicitly acquired and licensed from Licensor under another
+// license, the contents of this file are subject to the Reciprocal Public
+// License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+// and You may not copy or use this file in either source code or executable
+// form, except in compliance with the terms and conditions of the RPL.
+// 
+// All software distributed under the RPL is provided strictly on an "AS
+// IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+// LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+// LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+// language governing rights and limitations under the RPL.
+
+#endregion
+
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
@@ -5,125 +22,123 @@ using Microsoft.Azure.Documents.Client;
 
 namespace HQ.Cohort.Stores.Sql.DocumentDb
 {
-	public class CreateIdentitySchema
+    public class CreateIdentitySchema
     {
-	    private readonly DocumentClient _client;
-	    private readonly string _databaseId;
+        private readonly DocumentClient _client;
+        private readonly string _databaseId;
 
-	    public CreateIdentitySchema(DocumentClient client, string databaseId)
-	    {
-		    _client = client;
-		    _databaseId = databaseId;
-	    }
+        public CreateIdentitySchema(DocumentClient client, string databaseId)
+        {
+            _client = client;
+            _databaseId = databaseId;
+        }
 
-	    public async Task Up()
-	    {
-		    await CreateCollectionIfNotExistsAsync("AspNetIdentity");
+        public async Task Up()
+        {
+            await CreateCollectionIfNotExistsAsync("AspNetIdentity");
 
-			/*
-			Create.Table("AspNetRoles")
-			    .WithColumn("Id").AsString(450).NotNullable().PrimaryKey("PK_AspNetRoles")
-			    .WithColumn("Name").AsString(256).Nullable()
-			    .WithColumn("NormalizedName").AsString(256).Nullable()
-			    .WithColumn("ConcurrencyStamp").AsString(int.MaxValue).Nullable()
-			    ;
-			*/
+            /*
+            Create.Table("AspNetRoles")
+                .WithColumn("Id").AsString(450).NotNullable().PrimaryKey("PK_AspNetRoles")
+                .WithColumn("Name").AsString(256).Nullable()
+                .WithColumn("NormalizedName").AsString(256).Nullable()
+                .WithColumn("ConcurrencyStamp").AsString(int.MaxValue).Nullable()
+                ;
+            */
 
-			/*
-			Create.Table("AspNetUsers")
-			    .WithColumn("Id").AsString(450).NotNullable().PrimaryKey("PK_AspNetUsers")
-			    .WithColumn("UserName").AsString(256).Nullable()
-			    .WithColumn("NormalizedUserName").AsString(256).Nullable()
-			    .WithColumn("Email").AsString(256).Nullable()
-			    .WithColumn("NormalizedEmail").AsString(256).Nullable()
-			    .WithColumn("EmailConfirmed").AsBoolean().NotNullable()
-			    .WithColumn("PasswordHash").AsString(int.MaxValue).Nullable()
-			    .WithColumn("SecurityStamp").AsString(int.MaxValue).Nullable()
-			    .WithColumn("ConcurrencyStamp").AsString(int.MaxValue).Nullable()
-			    .WithColumn("PhoneNumber").AsString(int.MaxValue).Nullable()
-			    .WithColumn("PhoneNumberConfirmed").AsBoolean().NotNullable()
-			    .WithColumn("TwoFactorEnabled").AsBoolean().NotNullable()
-			    .WithColumn("LockoutEnd").AsDateTimeOffset().Nullable()
-			    .WithColumn("LockoutEnabled").AsBoolean().NotNullable()
-			    .WithColumn("AccessFailedCount").AsInt32().NotNullable()
-				;
-			*/
+            /*
+            Create.Table("AspNetUsers")
+                .WithColumn("Id").AsString(450).NotNullable().PrimaryKey("PK_AspNetUsers")
+                .WithColumn("UserName").AsString(256).Nullable()
+                .WithColumn("NormalizedUserName").AsString(256).Nullable()
+                .WithColumn("Email").AsString(256).Nullable()
+                .WithColumn("NormalizedEmail").AsString(256).Nullable()
+                .WithColumn("EmailConfirmed").AsBoolean().NotNullable()
+                .WithColumn("PasswordHash").AsString(int.MaxValue).Nullable()
+                .WithColumn("SecurityStamp").AsString(int.MaxValue).Nullable()
+                .WithColumn("ConcurrencyStamp").AsString(int.MaxValue).Nullable()
+                .WithColumn("PhoneNumber").AsString(int.MaxValue).Nullable()
+                .WithColumn("PhoneNumberConfirmed").AsBoolean().NotNullable()
+                .WithColumn("TwoFactorEnabled").AsBoolean().NotNullable()
+                .WithColumn("LockoutEnd").AsDateTimeOffset().Nullable()
+                .WithColumn("LockoutEnabled").AsBoolean().NotNullable()
+                .WithColumn("AccessFailedCount").AsInt32().NotNullable()
+                ;
+            */
 
-			/*
-			Create.Table("AspNetRoleClaims")
-			    .WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey("PK_AspNetRoleClaims")
-			    .WithColumn("RoleId").AsString(450).NotNullable().ForeignKey("FK_AspNetRoleClaims_AspNetRoles_RoleId", "AspNetRoles", "Id").OnDelete(Rule.Cascade)
-			    .WithColumn("ClaimType").AsString(int.MaxValue).Nullable()
-			    .WithColumn("ClaimValue").AsString(int.MaxValue).Nullable()
-			    ;
-			*/
+            /*
+            Create.Table("AspNetRoleClaims")
+                .WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey("PK_AspNetRoleClaims")
+                .WithColumn("RoleId").AsString(450).NotNullable().ForeignKey("FK_AspNetRoleClaims_AspNetRoles_RoleId", "AspNetRoles", "Id").OnDelete(Rule.Cascade)
+                .WithColumn("ClaimType").AsString(int.MaxValue).Nullable()
+                .WithColumn("ClaimValue").AsString(int.MaxValue).Nullable()
+                ;
+            */
 
-			/*
-			Create.Table("AspNetUserClaims")
-			    .WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey("PK_AspNetUserClaims")
-			    .WithColumn("UserId").AsString(450).NotNullable().ForeignKey("FK_AspNetUserClaims_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
-			    .WithColumn("ClaimType").AsString(int.MaxValue).Nullable()
-			    .WithColumn("ClaimValue").AsString(int.MaxValue).Nullable()
-			    ;
-			*/
+            /*
+            Create.Table("AspNetUserClaims")
+                .WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey("PK_AspNetUserClaims")
+                .WithColumn("UserId").AsString(450).NotNullable().ForeignKey("FK_AspNetUserClaims_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
+                .WithColumn("ClaimType").AsString(int.MaxValue).Nullable()
+                .WithColumn("ClaimValue").AsString(int.MaxValue).Nullable()
+                ;
+            */
 
-			/*
-			Create.Table("AspNetUserLogins")
-			    .WithColumn("LoginProvider").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserLogins")
-			    .WithColumn("ProviderKey").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserLogins")
-			    .WithColumn("ProviderDisplayName").AsString(int.MaxValue).Nullable()
-			    .WithColumn("UserId").AsString(450).NotNullable().ForeignKey("FK_AspNetUserLogins_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
-			    ;
-			*/
+            /*
+            Create.Table("AspNetUserLogins")
+                .WithColumn("LoginProvider").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserLogins")
+                .WithColumn("ProviderKey").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserLogins")
+                .WithColumn("ProviderDisplayName").AsString(int.MaxValue).Nullable()
+                .WithColumn("UserId").AsString(450).NotNullable().ForeignKey("FK_AspNetUserLogins_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
+                ;
+            */
 
-			/*
-			Create.Table("AspNetUserRoles")
-			    .WithColumn("UserId").AsString(450).NotNullable()
-				    .PrimaryKey("PK_AspNetUserRoles")
-				    .ForeignKey("FK_AspNetUserRoles_AspNetRoles_RoleId", "AspNetRoles", "Id").OnDelete(Rule.Cascade)
-			    .WithColumn("RoleId").AsString(450).NotNullable()
-				    .PrimaryKey("PK_AspNetUserRoles")
-				    .ForeignKey("FK_AspNetUserRoles_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
-			    ;
-			*/
+            /*
+            Create.Table("AspNetUserRoles")
+                .WithColumn("UserId").AsString(450).NotNullable()
+                    .PrimaryKey("PK_AspNetUserRoles")
+                    .ForeignKey("FK_AspNetUserRoles_AspNetRoles_RoleId", "AspNetRoles", "Id").OnDelete(Rule.Cascade)
+                .WithColumn("RoleId").AsString(450).NotNullable()
+                    .PrimaryKey("PK_AspNetUserRoles")
+                    .ForeignKey("FK_AspNetUserRoles_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
+                ;
+            */
 
-			/*
-			Create.Table("AspNetUserTokens")
-			    .WithColumn("UserId").AsString(450).NotNullable().PrimaryKey("PK_AspNetUserTokens").ForeignKey("FK_AspNetUserTokens_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
-			    .WithColumn("LoginProvider").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserTokens")
-			    .WithColumn("Name").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserTokens")
-			    .WithColumn("Value").AsString(int.MaxValue).Nullable()
-				;
-			*/
+            /*
+            Create.Table("AspNetUserTokens")
+                .WithColumn("UserId").AsString(450).NotNullable().PrimaryKey("PK_AspNetUserTokens").ForeignKey("FK_AspNetUserTokens_AspNetUsers_UserId", "AspNetUsers", "Id").OnDelete(Rule.Cascade)
+                .WithColumn("LoginProvider").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserTokens")
+                .WithColumn("Name").AsString(128).NotNullable().PrimaryKey("PK_AspNetUserTokens")
+                .WithColumn("Value").AsString(int.MaxValue).Nullable()
+                ;
+            */
 
-			/*
-			Create.Index("IX_AspNetRoleClaims_RoleId").OnTable("AspNetRoleClaims").OnColumn("RoleId");
-		    Create.Index("RoleNameIndex").OnTable("AspNetRoles").OnColumn("NormalizedName").Unique();
-			Create.Index("IX_AspNetUserClaims_UserId").OnTable("AspNetUserClaims").OnColumn("UserId");
-			Create.Index("IX_AspNetUserLogins_UserId").OnTable("AspNetUserLogins").OnColumn("UserId");
-			Create.Index("IX_AspNetUserRoles_RoleId").OnTable("AspNetUserRoles").OnColumn("RoleId");
-			Create.Index("EmailIndex").OnTable("AspNetUsers").OnColumn("NormalizedEmail");
-		    Create.Index("UserNameIndex").OnTable("AspNetUsers").OnColumn("NormalizedUserName").Unique();
-			*/
-		}
-		
-	    private async Task CreateCollectionIfNotExistsAsync(string collectionId)
-	    {
-		    try
-		    {
-			    await _client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(_databaseId, collectionId));
-		    }
-		    catch (DocumentClientException e)
-		    {
-			    if (e.StatusCode == HttpStatusCode.NotFound)
-			    {
-				    await _client.CreateDocumentCollectionAsync(UriFactory.CreateDatabaseUri(_databaseId), new DocumentCollection { Id = collectionId }, new RequestOptions { OfferThroughput = 400 });
-			    }
-			    else
-			    {
-				    throw;
-			    }
-		    }
-	    }
-	}
+            /*
+            Create.Index("IX_AspNetRoleClaims_RoleId").OnTable("AspNetRoleClaims").OnColumn("RoleId");
+            Create.Index("RoleNameIndex").OnTable("AspNetRoles").OnColumn("NormalizedName").Unique();
+            Create.Index("IX_AspNetUserClaims_UserId").OnTable("AspNetUserClaims").OnColumn("UserId");
+            Create.Index("IX_AspNetUserLogins_UserId").OnTable("AspNetUserLogins").OnColumn("UserId");
+            Create.Index("IX_AspNetUserRoles_RoleId").OnTable("AspNetUserRoles").OnColumn("RoleId");
+            Create.Index("EmailIndex").OnTable("AspNetUsers").OnColumn("NormalizedEmail");
+            Create.Index("UserNameIndex").OnTable("AspNetUsers").OnColumn("NormalizedUserName").Unique();
+            */
+        }
+
+        private async Task CreateCollectionIfNotExistsAsync(string collectionId)
+        {
+            try
+            {
+                await _client.ReadDocumentCollectionAsync(
+                    UriFactory.CreateDocumentCollectionUri(_databaseId, collectionId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound)
+                    await _client.CreateDocumentCollectionAsync(UriFactory.CreateDatabaseUri(_databaseId),
+                        new DocumentCollection {Id = collectionId}, new RequestOptions {OfferThroughput = 400});
+                else
+                    throw;
+            }
+        }
+    }
 }
