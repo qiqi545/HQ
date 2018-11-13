4f8f389f30c79ea687297f7e97158c9d99127289
Daniel Crenna <daniel@hq.io>
2018-11-13 16:39:03 -0500

    update to latest and fix script issues with submodule update

---
+++ rebuild.cmd
@@ -0,0 +1,26 @@
+@ECHO OFF
+
+REM use .gitconfig for this repository
+git config --local include.path /.gitconfig
+
+REM switch all submodules to master
+git submodule foreach git checkout master
+
+REM update all submodules to use tips of master branches
+git submodule foreach git pull origin master
+
+REM remove all source shadow copies
+FOR /D %%p IN ("src\HQ\*.*") DO rmdir "%%p" /s /q
+FOR /D %%p IN ("src\HQ.DocumentDb\*.*") DO rmdir "%%p" /s /q
+FOR /D %%p IN ("src\HQ.MySql\*.*") DO rmdir "%%p" /s /q
+FOR /D %%p IN ("src\HQ.Sqlite\*.*") DO rmdir "%%p" /s /q
+FOR /D %%p IN ("src\HQ.SqlServer\*.*") DO rmdir "%%p" /s /q
+
+REM need to move into the directory for MSBuild parameters to work
+cd src
+
+REM build once for merge source, and again for compile
+START /WAIT dotnet build -c Debug
+START /WAIT dotnet build -c Debug
+
+cd..
