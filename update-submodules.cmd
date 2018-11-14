3d0cdd8b0cdd2c8cd0e114e965dbe86f0299446a
Daniel Crenna <daniel@hq.io>
2018-11-14 18:00:57 -0500

    update build collateral

--- update-submodules.cmd
+++ update-submodules.cmd
@@ -1,10 +1,10 @@
 @ECHO OFF
 
-REM first time use
-git submodule update --init --recursive
-
 REM use .gitconfig for this repository
 git config --local include.path /.gitconfig
+
+REM first time use
+git submodule update --init --recursive
 
 REM switch all submodules to master
 git submodule foreach git checkout master
