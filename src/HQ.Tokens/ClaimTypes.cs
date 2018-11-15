f820d230dddffbd9f792538fbe85aea8ff236c5b
Daniel Crenna <daniel@hq.io>
2018-11-15 13:42:17 -0500

    add prefix temporarily to get around a role transform bug in .NET

--- src/HQ.Tokens/ClaimTypes.cs
+++ src/HQ.Tokens/ClaimTypes.cs
@@ -19,13 +19,13 @@ namespace HQ.Tokens
 {
     public static class ClaimTypes
     {
-        public const string AccountId = "account_id";
-        public const string AccountName = "account_name";
-        public const string ApplicationId = "app_id";
-        public const string ApplicationName = "app_name";
-        public const string UserId = "user_id";
-        public const string UserName = "user_name";
-        public const string Role = "role";
-        public const string Permission = "permission";
+        public const string AccountId = "app.account_id";
+        public const string AccountName = "app.account_name";
+        public const string ApplicationId = "app.app_id";
+        public const string ApplicationName = "app.app_name";
+        public const string UserId = "app.user_id";
+        public const string UserName = "app.user_name";
+        public const string Role = "app.role";
+        public const string Permission = "app.permission";
     }
 }
