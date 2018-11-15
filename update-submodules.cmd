e6772cba52054120d2d88335aac1488b3b5d6be9
Daniel Crenna <daniel@hq.io>
2018-11-15 14:53:03 -0500

    update build collateral

--- update-submodules.cmd
+++ update-submodules.cmd
@@ -3,8 +3,11 @@
 REM use .gitconfig for this repository
 git config --local include.path /.gitconfig
 
+REM first time use
+git submodule update --init --recursive
+
+REM switch all submodules to master
+git submodule foreach git checkout master
+
 REM update all submodules to use tips of master branches
 git submodule foreach git pull origin master
-
-REM recursively update the tips of master branches
-git submodule update --recursive --remote
