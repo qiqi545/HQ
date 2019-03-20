2e5a58e33565c8fe7fc3a2d6ceb8a23b588298c6
Daniel Crenna <daniel.crenna@gmail.com>
2019-03-20 15:30:22 -0400

    add theming project

---
+++ src/Blowdart.UI.Theming/UiTheme.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Blowdart, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Blowdart.UI.Theming
+{
+    public class UiTheme
+    {
+        public static UiTheme Default = new UiTheme();
+
+        public ColorVariables ColorVariables { get; }
+        public ImageVariables ImageVariables { get; }
+        
+        public UiTheme()
+        {
+            ColorVariables = new ColorVariables();
+            ImageVariables = new ImageVariables();
+        }
+    }
+}
