a318394dd2634b669aadce6511741ad5d6d80ebc
Daniel Crenna <daniel.crenna@gmail.com>
2019-04-03 09:53:58 -0400

    add code

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Demo
+﻿using Blowdart.UI;
+using Blowdart.UI.Web;
+using Blowdart.UI.Web.SemanticUI;
+
+namespace Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            UiConfig.Settings = site =>
+            {
+                site.Title = "AntHill";
+                site.System = new SemanticUi();
+            };
+
+            UiServer.Start(args, layout =>
+            {
+                layout.Default(ui =>
+                {
+                    ui.P("Hello, World!");
+                });
+            });
         }
     }
 }
