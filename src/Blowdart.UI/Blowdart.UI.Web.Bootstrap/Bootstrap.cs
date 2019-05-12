024be6dbbbe6f17beb701e6df9345eab292e1a4f
Daniel Crenna <daniel.crenna@gmail.com>
2019-05-12 15:15:10 -0400

    remove duplicated effort in other libs; add Bootstrap library stub

---
+++ src/Blowdart.UI/Blowdart.UI.Web.Bootstrap/Bootstrap.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Blowdart, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Blowdart.UI.Web.Bootstrap
+{
+    public class Bootstrap : HtmlSystem
+    {
+        public override string StylesSection()
+        {
+            const string components = @"
+<!-- Bootstrap -->
+<link rel=""stylesheet"" type=""text/css"" href=""~/lib/bootstrap/bootstrap-min.css"">";
+            return components;
+        }
+
+        public override string ScriptsSection()
+        {
+	        const string scripts = @"
+<!-- Bootstrap -->
+<script type=""text/javascript"" src=""~/lib/bootstrap/dist/bootstrap.min.js""></script>";
+            return scripts;
+        }
+    }
+}
