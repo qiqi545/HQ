73e7c723dd518c57c1366e4b9736cf6bb23d4fc0
Daniel Crenna <daniel.crenna@gmail.com>
2019-05-21 17:03:04 -0400

    Create ViewComponentAttribute.cs

---
+++ src/Blowdart.UI/Blowdart.UI/ViewComponentAttribute.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Blowdart, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Blowdart.UI
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+	public class ViewComponentAttribute : Attribute
+	{
+		public Type Type { get; }
+
+		public ViewComponentAttribute(Type type)
+		{
+			Type = type;
+		}
+	}
+}
