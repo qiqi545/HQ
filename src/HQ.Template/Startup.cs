9ad72cb6ab3efb3fd68577e8ebe8353cbc7ab5ec
Daniel Crenna <daniel.crenna@gmail.com>
2019-05-26 14:01:31 -0400

    add environment to installation

--- src/HQ.Template/Startup.cs
+++ src/HQ.Template/Startup.cs
@@ -20,26 +20,28 @@ namespace HQ.Template
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _environment;
 
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             _configuration = configuration;
+            _environment = environment;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
 #if DocumentDb
-            services.AddHq(DatabaseType.DocumentDb, _configuration.GetSection("HQ"))
+            services.AddHq(_environment,DatabaseType.DocumentDb, _configuration.GetSection("HQ"))
                 .AddSecurityPolicies(_configuration.GetSection("HQ").GetSection("Security"))
                 .AddGenerated<DocumentDbBatchOptions>(_configuration.GetSection("HQ").GetSection("Security"), "/api")
 #elif SqlServer
-            services.AddHq(DatabaseType.SqlServer, _configuration.GetSection("HQ"))
+            services.AddHq(_environment,DatabaseType.SqlServer, _configuration.GetSection("HQ"))
                 .AddSecurityPolicies(_configuration.GetSection("HQ").GetSection("Security"))
                 .AddGenerated<SqlServerOptions>(_configuration.GetSection("HQ").GetSection("Security"), "/api")
 #else
-            services.AddHq(DatabaseType.Sqlite, _configuration.GetSection("HQ"))
+            services.AddHq(_environment, DatabaseType.Sqlite, _configuration.GetSection("HQ"))
                 .AddSecurityPolicies(_configuration.GetSection("HQ").GetSection("Security"))
-                //.AddGenerated<SqliteOptions>(_configuration.GetSection("HQ").GetSection("Security"), "/api")
+                .AddGenerated<SqliteOptions>(_configuration.GetSection("HQ").GetSection("Security"), "/api")
 #endif
 
 #if AppInsights
