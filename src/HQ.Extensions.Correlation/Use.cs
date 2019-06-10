d158124cd67cfcb180c7ac6c76cd73c0b1af3fa6
Daniel Crenna <daniel.crenna@gmail.com>
2019-06-09 20:04:52 -0400

    nit: formatting

--- src/HQ.Extensions.Correlation/Use.cs
+++ src/HQ.Extensions.Correlation/Use.cs
@@ -1,3 +1,20 @@
+#region LICENSE
+
+// Unless explicitly acquired and licensed from Licensor under another
+// license, the contents of this file are subject to the Reciprocal Public
+// License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+// and You may not copy or use this file in either source code or executable
+// form, except in compliance with the terms and conditions of the RPL.
+// 
+// All software distributed under the RPL is provided strictly on an "AS
+// IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+// LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+// LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+// language governing rights and limitations under the RPL.
+
+#endregion
+
 using HQ.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -13,12 +30,17 @@ namespace HQ.Extensions.Correlation
             app.Use(async (context, next) =>
             {
                 if (!context.Request.Headers.TryGetValue(Constants.HttpHeaders.TraceParent, out var traceContext))
-                    context.Request.Headers.Add(Constants.HttpHeaders.TraceParent, traceContext = TraceContext.New().Header);
+                {
+                    context.Request.Headers.Add(Constants.HttpHeaders.TraceParent,
+                        traceContext = TraceContext.New().Header);
+                }
 
                 context.TraceIdentifier = traceContext;
 
                 if (app.ApplicationServices.GetService(typeof(IHttpContextAccessor)) is IHttpContextAccessor accessor)
+                {
                     accessor.HttpContext = context;
+                }
 
                 await next();
             });
