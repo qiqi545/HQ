916dbd08229f03692ccd93cf7b6a0f90eb69aade
Daniel Crenna <daniel.crenna@gmail.com>
2019-07-15 18:07:26 -0400

    v1.0.93

--- test/HQ.Extensions.Caching.Tests/Options/Sqlite/SaveOptionsSqliteFixture.cs
+++ test/HQ.Extensions.Caching.Tests/Options/Sqlite/SaveOptionsSqliteFixture.cs
@@ -1,5 +1,4 @@
-﻿using HQ.Integration.Sqlite.Sql;
-using HQ.Test.Sdk.Data;
+﻿using HQ.Test.Sdk.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +9,6 @@ namespace HQ.Extensions.Tests.Options.Sqlite
 		public override void ConfigureServices(IServiceCollection services)
 		{
 			var configBuilder = new ConfigurationBuilder();
-			configBuilder.AddSqlite();
 		}
 	}
 }
