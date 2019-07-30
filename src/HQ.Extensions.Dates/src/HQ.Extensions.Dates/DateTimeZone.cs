1f5c3bd7c1f3b58cb6937d42141bc91a1cf49d8a
Daniel Crenna <daniel.crenna@gmail.com>
2019-07-30 11:34:10 -0400

    fix degenerate conversion from coordinated universal time

--- src/HQ.Extensions.Dates/src/HQ.Extensions.Dates/DateTimeZone.cs
+++ src/HQ.Extensions.Dates/src/HQ.Extensions.Dates/DateTimeZone.cs
@@ -14,7 +14,6 @@
 #endregion
 
 using System;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -22,161 +21,181 @@ using TimeZoneConverter;
 
 namespace HQ.Extensions.Dates
 {
-    /// <summary>
-    /// A <see cref="DateTimeOffset"/> replacement that doesn't lose time zone information, and normalizes to IANA time zone names.
-    /// </summary>
-    [DebuggerDisplay("{DisplayName}")]
-    public struct DateTimeZone : IEquatable<DateTimeZone>, IEquatable<DateTimeOffset>, IComparable<DateTimeZone>, IComparable<DateTimeOffset>, IFormattable, IComparable, ISerializable, IDeserializationCallback
-    {
-        private readonly DateTimeOffset _instant;
-        private readonly TimeZoneInfo _timeZone;
+	/// <summary>
+	/// A <see cref="DateTimeOffset"/> replacement that doesn't lose time zone information,
+	/// and normalizes to IANA time zone names.
+	/// </summary>
+	public struct DateTimeZone : IEquatable<DateTimeZone>, IEquatable<DateTimeOffset>, IComparable<DateTimeZone>, IComparable<DateTimeOffset>, IFormattable, IComparable, ISerializable, IDeserializationCallback
+	{
+		private readonly DateTimeOffset _instant;
+		private readonly TimeZoneInfo _timeZone;
 
-        public static DateTimeZone Now => new DateTimeZone(DateTimeOffset.UtcNow, TimeZoneInfo.Local);
-        public static DateTimeZone UtcNow => new DateTimeZone(DateTimeOffset.UtcNow, TimeZoneInfo.Utc);
+		public static DateTimeZone Now => new DateTimeZone(DateTimeOffset.UtcNow, TimeZoneInfo.Local);
+		public static DateTimeZone UtcNow => new DateTimeZone(DateTimeOffset.UtcNow, TimeZoneInfo.Utc);
 
-        public DateTimeOffset DateTimeOffset => TimeZoneInfo.ConvertTime(_instant, _timeZone);
-        
-        /// <summary> Creates a <see cref="DateTimeZone"/> instance based on the most prevalent time zone associated with the provided instant's time zone offset </summary>
-        public DateTimeZone(DateTimeOffset instant, string ianaOrWindowsTimeZoneId)
-        {
-            if(!TZConvert.TryGetTimeZoneInfo(ianaOrWindowsTimeZoneId, out _timeZone))
-                throw new ArgumentException("The provided time zone identifier was not recognized.", nameof(ianaOrWindowsTimeZoneId));
-            _instant = instant.ToUniversalTime();
-        }
+		public DateTimeOffset DateTimeOffset => TimeZoneInfo.ConvertTime(_instant, _timeZone);
 
-        /// <summary> Creates a <see cref="DateTimeZone"/> instance based on the most prevalent time zone associated with the provided instant's time zone offset </summary>
-        public DateTimeZone(DateTimeOffset instant, TimeZoneInfo timeZone)
-        {
-            _instant = instant.ToUniversalTime();
-            _timeZone = timeZone;
-        }
+		/// <summary> Creates a <see cref="DateTimeZone"/> instance based on the most prevalent time zone associated with the provided instant's time zone offset </summary>
+		public DateTimeZone(DateTimeOffset instant, string ianaOrWindowsTimeZoneId)
+		{
+			if (!TryGetTimeZoneInfo(ianaOrWindowsTimeZoneId, out _timeZone))
+				throw new ArgumentException("The provided time zone identifier was not recognized.", nameof(ianaOrWindowsTimeZoneId));
+			_instant = instant.ToUniversalTime();
+		}
 
-        public static implicit operator DateTimeOffset(DateTimeZone dateTimeZone) => dateTimeZone.DateTimeOffset;
-        public static explicit operator DateTimeZone(DateTimeOffset dateTimeOffset)
-        {
-            if (dateTimeOffset.Offset == TimeSpan.Zero)
-                throw new ArgumentException("Cannot cast a DateTimeOffset to DateTimeZone with no offset",
-                    nameof(dateTimeOffset));
+		/// <summary> Creates a <see cref="DateTimeZone"/> instance based on the most prevalent time zone associated with the provided instant's time zone offset </summary>
+		public DateTimeZone(DateTimeOffset instant, TimeZoneInfo timeZone)
+		{
+			_instant = instant.ToUniversalTime();
+			_timeZone = timeZone;
+		}
 
-            foreach (var candidate in TZConvert.KnownWindowsTimeZoneIds.Select(TZConvert.GetTimeZoneInfo))
-            {
-                if (candidate.BaseUtcOffset != dateTimeOffset.Offset)
-                    continue;
+		public static implicit operator DateTimeOffset(DateTimeZone dateTimeZone) => dateTimeZone.DateTimeOffset;
+		public static explicit operator DateTimeZone(DateTimeOffset dateTimeOffset)
+		{
+			if (dateTimeOffset.Offset == TimeSpan.Zero)
+				throw new ArgumentException("Cannot cast a DateTimeOffset to DateTimeZone with no offset",
+					nameof(dateTimeOffset));
 
-                return TZConvert.TryWindowsToIana(candidate.Id, CultureInfo.CurrentCulture.TwoLetterISOLanguageName,
-                           out var iana) && TZConvert.TryGetTimeZoneInfo(iana, out var result)
-                    ? new DateTimeZone(dateTimeOffset, result)
-                    : new DateTimeZone(dateTimeOffset, candidate);
-            }
+			foreach (var candidate in TZConvert.KnownWindowsTimeZoneIds.Select(TZConvert.GetTimeZoneInfo))
+			{
+				if (candidate.BaseUtcOffset != dateTimeOffset.Offset)
+					continue;
 
-            throw new NotSupportedException("Could not derive a time zone from the specified offset");
-        }
+				var windows = candidate.Id;
+				var territoryCode = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
 
-        #region Equality
+				return TZConvert.TryWindowsToIana(windows, territoryCode, out var iana) && TryGetTimeZoneInfo(iana, out var result)
+					? new DateTimeZone(dateTimeOffset, result)
+					: new DateTimeZone(dateTimeOffset, candidate);
+			}
 
-        public bool Equals(DateTimeZone other)
-        {
-            return DateTimeOffset.Equals(other.DateTimeOffset);
-        }
+			throw new NotSupportedException("Could not derive a time zone from the specified offset");
+		}
 
-        public int CompareTo(DateTimeOffset other)
-        {
-            return DateTimeOffset.CompareTo(other);
-        }
+		private static bool TryGetTimeZoneInfo(string ianaOrWindowsTimeZoneId, out TimeZoneInfo timeZoneInfo)
+		{
+			if (!TZConvert.TryGetTimeZoneInfo(ianaOrWindowsTimeZoneId, out timeZoneInfo))
+			{
+				if (!string.IsNullOrWhiteSpace(ianaOrWindowsTimeZoneId) &&
+					ianaOrWindowsTimeZoneId == "Coordinated Universal Time" ||
+					ianaOrWindowsTimeZoneId == TimeZoneInfo.Utc.StandardName)
+				{
+					timeZoneInfo = TimeZoneInfo.Utc;
+					return true;
+				}
 
-        public bool Equals(DateTimeOffset other)
-        {
-            return DateTimeOffset.Equals(other);
-        }
+				return false;
+			}
 
-        public override bool Equals(object obj)
-        {
-            return obj is DateTimeZone other && Equals(other);
-        }
+			return true;
+		}
 
-        public override int GetHashCode()
-        {
-            return DateTimeOffset.GetHashCode();
-        }
+		#region Equality
 
-        public static bool operator ==(DateTimeZone left, DateTimeZone right)
-        {
-            return left.Equals(right);
-        }
+		public bool Equals(DateTimeZone other)
+		{
+			return DateTimeOffset.Equals(other.DateTimeOffset);
+		}
 
-        public static bool operator !=(DateTimeZone left, DateTimeZone right)
-        {
-            return !left.Equals(right);
-        }
+		public int CompareTo(DateTimeOffset other)
+		{
+			return DateTimeOffset.CompareTo(other);
+		}
 
-        #endregion
+		public bool Equals(DateTimeOffset other)
+		{
+			return DateTimeOffset.Equals(other);
+		}
 
-        #region Comparison
+		public override bool Equals(object obj)
+		{
+			return obj is DateTimeZone other && Equals(other);
+		}
 
-        public int CompareTo(DateTimeZone other)
-        {
-            return DateTimeOffset.CompareTo(other.DateTimeOffset);
-        }
+		public override int GetHashCode()
+		{
+			return DateTimeOffset.GetHashCode();
+		}
 
-        public int CompareTo(object obj)
-        {
-            return ReferenceEquals(null, obj) ? 1 :
-                obj is DateTimeZone other ? CompareTo(other) :
-                throw new ArgumentException($"Object must be of type {nameof(DateTimeZone)}");
-        }
+		public static bool operator ==(DateTimeZone left, DateTimeZone right)
+		{
+			return left.Equals(right);
+		}
 
-        public static bool operator <(DateTimeZone left, DateTimeZone right)
-        {
-            return left.CompareTo(right) < 0;
-        }
+		public static bool operator !=(DateTimeZone left, DateTimeZone right)
+		{
+			return !left.Equals(right);
+		}
 
-        public static bool operator >(DateTimeZone left, DateTimeZone right)
-        {
-            return left.CompareTo(right) > 0;
-        }
+		#endregion
 
-        public static bool operator <=(DateTimeZone left, DateTimeZone right)
-        {
-            return left.CompareTo(right) <= 0;
-        }
+		#region Comparison
 
-        public static bool operator >=(DateTimeZone left, DateTimeZone right)
-        {
-            return left.CompareTo(right) >= 0;
-        }
+		public int CompareTo(DateTimeZone other)
+		{
+			return DateTimeOffset.CompareTo(other.DateTimeOffset);
+		}
 
-        #endregion
+		public int CompareTo(object obj)
+		{
+			return ReferenceEquals(null, obj) ? 1 :
+				obj is DateTimeZone other ? CompareTo(other) :
+				throw new ArgumentException($"Object must be of type {nameof(DateTimeZone)}");
+		}
 
-        #region Formatting
+		public static bool operator <(DateTimeZone left, DateTimeZone right)
+		{
+			return left.CompareTo(right) < 0;
+		}
 
-        public override string ToString() => DisplayName;
+		public static bool operator >(DateTimeZone left, DateTimeZone right)
+		{
+			return left.CompareTo(right) > 0;
+		}
 
-        private string DisplayName => $"{DateTimeOffset} [{TimeZone}]";
+		public static bool operator <=(DateTimeZone left, DateTimeZone right)
+		{
+			return left.CompareTo(right) <= 0;
+		}
 
-        public void OnDeserialization(object sender)
-        {
-            ((IDeserializationCallback) DateTimeOffset).OnDeserialization(sender);
-        }
+		public static bool operator >=(DateTimeZone left, DateTimeZone right)
+		{
+			return left.CompareTo(right) >= 0;
+		}
 
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            ((ISerializable) DateTimeOffset).GetObjectData(info, context);
-        }
+		#endregion
 
-        private string TimeZone => TZConvert.WindowsToIana(_timeZone.StandardName);
-        
-        public string ToString(string format, IFormatProvider formatProvider)
-        {
-            return DateTimeOffset.ToString(format, formatProvider);
-        }
+		#region Formatting
 
-        #endregion
+		public override string ToString() => DisplayName;
 
-        #region DateTimeOffset Passthrough
+		private string DisplayName => $"{DateTimeOffset} [{TimeZone}]";
 
-        public DateTime UtcDateTime => DateTimeOffset.UtcDateTime;
-        public TimeSpan Offset => DateTimeOffset.Offset;
+		public void OnDeserialization(object sender)
+		{
+			((IDeserializationCallback) DateTimeOffset).OnDeserialization(sender);
+		}
 
-        #endregion
-    }
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			((ISerializable) DateTimeOffset).GetObjectData(info, context);
+		}
+
+		public string TimeZone => TZConvert.WindowsToIana(_timeZone.StandardName);
+
+		public string ToString(string format, IFormatProvider formatProvider)
+		{
+			return DateTimeOffset.ToString(format, formatProvider);
+		}
+
+		#endregion
+
+		#region DateTimeOffset Passthrough
+
+		public DateTime UtcDateTime => DateTimeOffset.UtcDateTime;
+		public TimeSpan Offset => DateTimeOffset.Offset;
+
+		#endregion
+	}
 }
