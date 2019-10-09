c210ab617595808aab544fa76c13099fa3d7a9db
Daniel Crenna <daniel.crenna@gmail.com>
2019-10-08 22:24:30 -0400

    remove unused build tools

--- clean.cmd
+++
@@ -1,7 +0,0 @@
-REM Recursively remove /obj and /bin folders
-@ECHO OFF
-rmdir /s /q bin\lib\
-pushd src
-for /f "tokens=*" %%i in ('DIR /B /AD /S obj') do rmdir /s /q %%i
-for /f "tokens=*" %%i in ('DIR /B /AD /S bin') do rmdir /s /q %%i
-popd
